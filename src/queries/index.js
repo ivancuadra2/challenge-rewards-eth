bb3420931b202a8bbcbbdda703575c9317fd96a8
XINC02 <icuadrado@pan-energy.com>
2024-10-28 00:22:56 -0300

    Correccion de la llamada por un error

--- src/queries/index.js
+++ src/queries/index.js
@@ -7,19 +7,23 @@ const client = new ApolloClient({
       'X-API-KEY': 'BQY7FhzuJTR0jFiNW7CZmVEeDUZT8raU'
     }
   });
-  import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
-
-export const GET_ETH_REWARDS = gql`
-  query GetEthRewards($since: String!, $till: String!) {
-    ethereum {
-      blocks {
-        date {
-          date
+  
+  
+  // Modifica la consulta para aceptar variables
+  const GET_ETH_REWARDS = gql`
+    query GetEthRewards($since: ISO8601DateTime!, $till: ISO8601DateTime!) {
+      ethereum {
+        blocks(
+          date: {since: $since, till: $till}
+          options: {limit: 30}
+        ) {
+          date {
+            date
+          }
+          reward
         }
-        reward
       }
     }
-  }
-`;
+  `;
 
   export {client, GET_ETH_REWARDS}
